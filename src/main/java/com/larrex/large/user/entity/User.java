727b6698695e8ce01fcc4f429bb16c451add6e27
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-01-03 03:59:49 +0100

    switch to mongo

--- src/main/java/com/larrex/large/user/entity/User.java
+++ src/main/java/com/larrex/large/user/entity/User.java
@@ -1,5 +1,33 @@
 package com.larrex.large.user.entity;
 
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Document
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String bio;
+    private String location;
+    private String profileImageUrl;
+    private String coverImageUrl;
+    private List<String> postCategory;
+
+
+    private Date createdAt;
+    private Date updatedAt;
+
+
 }
